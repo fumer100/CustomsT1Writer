f7def04d383b7b45f73d8f1e9c8c1de810f71fb0
Furkan Mert <49523353+fumer100@users.noreply.github.com>
2023-09-26 15:17:41 +0200

    Add restored Project via Upload

---
+++ TkinterYahee.py
@@ -0,0 +1,111 @@
+from tkinter import filedialog
+import tkinter as tk
+import yaheeFunctions as m
+import pandas as pd
+hauptFenster = tk.Tk()
+outpath = 'C:\\Users\\Furkan Mert\\Documents\\YAHEEZIEL\\'
+hauptFenster.geometry("800x500")
+hauptFenster.winfo_toplevel().title('YAHEE T1')
+instructionLabel = tk.Label(hauptFenster, text="Geben sie die Daten ein!")
+
+
+def deleteInput():
+    Sdus.delete(0, 'end')
+    Schiff.delete(0, 'end')
+    BL.delete(0, 'end')
+    BLDATUM.delete(0, 'end')
+    rechnungsnr.delete(0, 'end')
+    rechnungsdatum.delete(0, 'end')
+    Containernr.delete(0, 'end')
+    Incoterm.delete(0, 'end')
+    Transportpreis.delete(0, 'end')
+
+
+def createFile(Sdus, Schiff, BL, BLDATUM, rechnungsnr, rechnungsdatum, Containernr, Incoterm, Transportpreis,
+               Inlandspreis, Packliste):
+    Sendungsnr = Sdus.get()
+    Schiff1 = Schiff.get()
+    BL1 = BL.get()
+    BLDatum = BLDATUM.get()
+    Rechnungsnr = rechnungsnr.get()
+    Rechnungsdatum = rechnungsdatum.get()
+    Containernr1 = Containernr.get()
+    Incoterm1 = "FOB " + Incoterm.get()
+    Transportpreis1 = Transportpreis.get() + " EUR"
+    Inlandpreis1 = str(Inlandspreis) + " EUR"
+    writer = pd.ExcelWriter(outpath + Sendungsnr + ".xlsx", engine='xlsxwriter') #IMPORTANT!!!
+
+    T1, verzollung = m.createT1(Packliste)
+    m.createWorkbook(T1, verzollung, Sendungsnr, Schiff1, BL1, BLDatum, Rechnungsnr, Rechnungsdatum, Containernr1,
+                     Incoterm1, Transportpreis1, Inlandpreis1, writer)
+    print('done')
+    print("Created: "+Sendungsnr+".xlsx")
+    deleteInput()
+    return
+
+#per button auswahl
+def open_file_dialog():
+    global file_path
+    file_path = filedialog.askopenfilename(initialdir='D://file')
+    print("Ausgewählte Datei:", file_path)
+
+#per dnd auswahl
+#def drop(event):
+    #listb.insert("end",event.data)
+
+
+#listb = tk.Listbox(hauptFenster, selectmode=tk.SINGLE)
+#listb.drop_target_register(dnd.DND_FILES)
+#listb.bind("<<Drop>>",drop)
+#listb.grid(row=11, column=1)
+
+
+
+Sdus = tk.Entry(hauptFenster)
+Sdus.grid(row=0, column=1)
+tk.Label(hauptFenster, text='SDUS').grid(row=0)
+
+Schiff = tk.Entry(hauptFenster)
+Schiff.grid(row=1, column=1)
+tk.Label(hauptFenster, text='Schiff').grid(row=1)
+
+BL = tk.Entry(hauptFenster)
+BL.grid(row=2, column=1)
+tk.Label(hauptFenster, text='BL').grid(row=2)
+
+BLDATUM = tk.Entry(hauptFenster)
+BLDATUM.grid(row=3, column=1)
+tk.Label(hauptFenster, text='BLDatum').grid(row=3)
+
+rechnungsnr = tk.Entry(hauptFenster)
+rechnungsnr.grid(row=4, column=1)
+tk.Label(hauptFenster, text='Rechnungsnr.').grid(row=4)
+
+rechnungsdatum = tk.Entry(hauptFenster)
+rechnungsdatum.grid(row=5, column=1)
+tk.Label(hauptFenster, text='Rechnungsdatum').grid(row=5)
+
+Containernr = tk.Entry(hauptFenster)
+Containernr.grid(row=6, column=1)
+tk.Label(hauptFenster, text='Containernr.').grid(row=6)
+
+Incoterm = tk.Entry(hauptFenster)
+Incoterm.grid(row=7, column=1)
+tk.Label(hauptFenster, text='Incoterm').grid(row=7)
+
+Transportpreis = tk.Entry(hauptFenster)
+Transportpreis.grid(row=8, column=1)
+tk.Label(hauptFenster, text='Transportpreis').grid(row=8)
+
+Inlandspreis = 795
+
+SdusButton = tk.Button(text='Create',
+                       command=lambda: createFile(Sdus, Schiff, BL, BLDATUM, rechnungsnr, rechnungsdatum, Containernr,
+                                                  Incoterm, Transportpreis, Inlandspreis, file_path))
+SdusButton.grid(row=12, column=1)
+
+PacklistWahlButton = tk.Button(text='wähle Packliste',
+                               command=lambda: open_file_dialog())
+hauptFenster.update()
+PacklistWahlButton.grid(row=10, column=1)
+hauptFenster.mainloop()
---
+++ yaheeFunctions.py
@@ -0,0 +1,160 @@
+import pandas as pd
+
+
+def createT1(file):
+    # read xls ,save specific columns.
+    with pd.ExcelFile(file) as xls:
+        packList = pd.read_excel(xls, 'Packing List')
+        civ = pd.read_excel(xls, 'Commercial Invoice')
+
+    startIndexPacklist = 7
+    startIndexCiv = 8
+
+    civLen, packLen = getLengthOfColumns(packList, civ)
+    Itemlist = packList[startIndexPacklist:packLen]['Unnamed: 2'].reset_index(drop=True)  # ready
+    CTN = packList[startIndexPacklist:packLen]['Unnamed: 6'].reset_index(drop=True)  # ready
+    GrossWeight = packList[startIndexPacklist:packLen]['Unnamed: 9'].reset_index(drop=True)  # ready
+    NetWeight = packList[startIndexPacklist:packLen]['Unnamed: 11'].reset_index(drop=True)  # ready
+    CustomsValue3 = civ[startIndexCiv:7 + civLen]['Unnamed: 6'].reset_index(drop=True)  # ready
+    hsCode = civ[startIndexCiv:7 + civLen]['Unnamed: 9'].reset_index(drop=True)  # ready
+    # Erstellung der Tabelle
+
+    dict = {'AT/B Position': "", 'H.S code': hsCode, "Ursprung": "CN", 'Warenbeschreibung': Itemlist,
+            'Packstückanzahl': CTN, 'Gewicht': GrossWeight,
+            'Netto': NetWeight, 'Warenwert': CustomsValue3, "Besonderheit": "ydocs"}  # y-docs fehlen mit macro
+    df = pd.DataFrame(dict)
+
+    # Erstellung der T1
+    T1 = df.reset_index(drop=True)
+    print(T1)
+
+    # Erstellung der Verzollung
+    verzollung = createVerzollung(T1)
+    print(type(verzollung))
+    T1["Warenwert"] = [round(a, 2) for a in T1["Warenwert"]]
+
+    T1 = addYcodes(T1)
+    T1 = addSums(T1)
+    return T1, verzollung
+
+
+def writeToExcel(writer, verzollung, T1, Codes):  # DONT TOUCH
+    verzollung.to_excel(writer, sheet_name='verzollung', startrow=9, startcol=0, index=False)
+    T1.to_excel(writer, sheet_name='T1', startrow=9, startcol=0, index=False)
+    Codes.to_excel(writer, sheet_name='Codes', startrow=0, startcol=0, index=False)
+
+    return
+
+
+def createVerzollung(T1):  # DONT TOUCH #FINAL
+    join_unique = lambda x: ','.join(set(x))
+    test = T1.groupby('H.S code').agg({'Warenbeschreibung': join_unique})  # erzeugt Warenbeschreibung für jede HS
+    verzollung = T1.groupby(['H.S code']).sum()  # Summiert alles für jede HS
+    verzollung["Warenwert"] = [round(a, 2) for a in verzollung["Warenwert"]]
+    verzollung['Warenbeschreibung'] = test['Warenbeschreibung']  # ersetzt Warenbeschreibung mit richtiger.
+    verzollung['Ursprung'] = ["CN" for i in verzollung['Ursprung']]
+    verzollung['Besonderheit'] = ["y-docs" for i in verzollung['Besonderheit']]  # Change to y/code Macro later
+    verzollung = verzollung.reset_index()
+    verzollung = verzollung.set_index('AT/B Position')
+    # print("Atb als index",verzollung)
+    verzollung['index_column'] = verzollung.index
+    # print("Indexspalte geaddet ",verzollung)
+    verzollung = verzollung.reset_index()
+    verzollung = verzollung.set_index('index_column')
+    # print("indexspalte als neuer index",verzollung)
+    verzollung = addYcodes(verzollung)
+    verzollung = addSums(verzollung)
+
+    print(verzollung)
+    return verzollung
+
+
+def addSums(table):  # dont Touch
+    summs = ["", "", "", "Summe", sum(table['Packstückanzahl']), sum(table['Gewicht']), sum(table['Netto']),
+             sum(table['Warenwert']), ""]
+    table.loc[len(table)] = ["", "", "", '', '', '', '', "", ""]
+    table.loc[len(table) + 1] = ["", "", "", '', '', '', '', "", ""]
+    table.loc[len(table) + 2] = summs
+
+    return table
+
+
+def addYcodes(table):
+    table['Besonderheit'] = ["=SVERWEIS(B" + str(i) + "&C" + str(
+        i) + ";'https://clasquinsa.sharepoint.com/sites/CLQ-DUS/Shared Documents/General/DUS/Seefracht/Import Kunden/Yahee/Austarifierung/SDUS012030.xlsx'#$Codes.A$1:C$1048576;3;0)"
+                             for i in range(11, 11 + len(table))]
+
+    return table
+
+
+def getLengthOfColumns(packList, civ):  # DONT TOUCH #FINAL
+    lengthCiv = len(civ['Unnamed: 9'].dropna())
+    lengthPackList = len(packList['Unnamed: 6']) - 1
+    # print('lengthPacklist:',lengthPackList)
+    return lengthCiv, lengthPackList
+
+
+def createWorkbook(T1, verzollung, Sendungsnr, Schiff, BL, BLDatum, Rechnungsnr, RechnungsDatum, Containernr, Incoterm,
+                   Transportpreis, Inlandpreis, writer):
+    with pd.ExcelFile("Y-Docs.xls") as xls:
+        codes = pd.read_excel(xls, 'Codes')
+    writeToExcel(writer, verzollung, T1, codes)
+    print('verzollung', verzollung)
+
+    T1Sheet = writer.sheets['T1']
+    VerzollungsSheet = writer.sheets['verzollung']
+    T1Sheet.set_column(0, 10, 25)
+    VerzollungsSheet.set_column(0, 10, 25)
+
+    T1Sheet.write('A1', 'Sendungsnummer:')
+    T1Sheet.write('B1', Sendungsnr)
+
+    T1Sheet.write('A2', 'Schiff:')
+    T1Sheet.write('B2', Schiff)
+
+    T1Sheet.write('A3', 'B/L Nummer:')
+    T1Sheet.write('B3', BL)
+    T1Sheet.write('C3', BLDatum)
+
+    T1Sheet.write('A4', 'Rechnung:')
+    T1Sheet.write('B4', Rechnungsnr)
+    T1Sheet.write('C4', RechnungsDatum)
+
+    T1Sheet.write('A5', 'Container:')
+    T1Sheet.write('B5', Containernr)
+
+    T1Sheet.write('A6', 'Incoterm:')
+    T1Sheet.write('B6', Incoterm)
+
+    T1Sheet.write('A7', 'Transportpreis:')
+    T1Sheet.write('B7', Transportpreis)
+
+    T1Sheet.write('A8', 'Inland:')
+    T1Sheet.write('B8', Inlandpreis)
+
+    VerzollungsSheet.write('A1', 'Sendungsnummer:')
+    VerzollungsSheet.write('B1', Sendungsnr)
+
+    VerzollungsSheet.write('A2', 'Schiff:')
+    VerzollungsSheet.write('B2', Schiff)
+
+    VerzollungsSheet.write('A3', 'B/L Nummer:')
+    VerzollungsSheet.write('B3', BL)
+    VerzollungsSheet.write('C3', BLDatum)
+
+    VerzollungsSheet.write('A4', 'Rechnung:')
+    VerzollungsSheet.write('B4', Rechnungsnr)
+    VerzollungsSheet.write('C4', RechnungsDatum)
+
+    VerzollungsSheet.write('A5', 'Container:')
+    VerzollungsSheet.write('B5', Containernr)
+    VerzollungsSheet.write('A6', 'Incoterm:')
+    VerzollungsSheet.write('B6', Incoterm)
+
+    VerzollungsSheet.write('A7', 'Transportpreis:')
+    VerzollungsSheet.write('B7', Transportpreis)
+
+    VerzollungsSheet.write('A8', 'Inland:')
+    VerzollungsSheet.write('B8', Inlandpreis)
+    writer.close()
+    return
